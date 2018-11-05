0bbe6570da49a26e740b1342cb02ff40e3e7e647
FichteFoll <fichtefoll2@googlemail.com>
2018-11-05 23:24:27 +0100

    Fetch users to display from hash and add an input

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const hash2Obj = location.hash.substring(1)
+      .split("&")
+      .map(v => v.split("="))
+      .reduce( (pre, [key, value]) => ({ ...pre, [key]: value }), {} )
+
 const app = new Vue({
   el: '#app',
   data: {
     // would like to use Maps, but Vue doesn't support those in v-for
     // https://github.com/vuejs/vue/issues/2410
     sourceEntries: {},
-    entriesChangeTracker: 1,
     users: {},
+    usersInput: hash2Obj.users || ""
   },
   computed: {
     entries () {
-      this.entriesChangeTracker
       let dstEntries = new Map()
-      for (let [userName, srcEntries] of Object.entries(this.sourceEntries)) {
-        for (let {media, ...rest} of srcEntries) {
+      for (const [userName, srcEntries] of Object.entries(this.sourceEntries)) {
+        for (const {media, ...rest} of srcEntries) {
           if (!dstEntries.has(media.id)) {
             dstEntries.set(media.id, {media: media, users: new Map()})
           }
@@ -27,7 +31,6 @@ const app = new Vue({
       return ret
     },
     sortedUsers () {
-      this.entriesChangeTracker
       let userList = Array.from(Object.values(this.users))
       userList.sort((a, b) => {
         return a.name.localeCompare(b.name)
@@ -36,30 +39,40 @@ const app = new Vue({
     }
   },
   created () {
-    let userNames = ["FichteFoll", "Firebird97", "Gattix"]
-    getMediaLists(userNames, "CURRENT")
-      // TODO handle errors
-      .then(json => {
-        console.log("results for users", userNames, json)
-        for (const collection of Object.values(json.data)) {
-          // discard custom lists as they have duplicates
-          const user = collection.user
-          const entries = collection.lists.reduce(
-            (sum, x) => !x.isCustomList ? sum.concat(x.entries) : sum,
-            []
-          )
-          if (entries.length === 0) {
-            // TODO warn visibly
-            console.warn(`List for ${user.name} is empty!`)
-            return
-          }
-          this.sourceEntries[user.name] = entries
-          this.users[user.name] = user
-        }
-        this.entriesChangeTracker += 1
-      })
+    this.fetchLists()
   },
   onUsersInput() {
-    // location.replace()
-  }
+    this.fetchLists()
+  },
+  methods: {
+    fetchLists () {
+      console.log("fetching", this.usersInput)
+      if (this.usersInput === "") return;
+      // or location.assign()
+      location.replace(location.origin + location.pathname + location.search
+                       + "#users=" + this.usersInput)
+      const userNames = this.usersInput.split(",")
+      document.title = `AniTogether - ${userNames.join(", ")}`
+      getMediaLists(userNames, "CURRENT")
+        // TODO handle errors
+        .then(json => {
+          console.log("results for users", userNames, json)
+          for (const collection of Object.values(json.data)) {
+            // discard custom lists as they have duplicates
+            const user = collection.user
+            const entries = collection.lists.reduce(
+              (sum, x) => !x.isCustomList ? sum.concat(x.entries) : sum,
+              []
+            )
+            if (entries.length === 0) {
+              // TODO warn visibly
+              console.warn(`List for ${user.name} is empty!`)
+              return
+            }
+            Vue.set(this.sourceEntries, user.name, entries)
+            Vue.set(this.users, user.name, user)
+          }
+        })
+    }
+  },
 })
